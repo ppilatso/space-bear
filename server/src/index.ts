d1e87e1b476205e78d771221a4660086e8ecea9d
Phenyo Pilatso <pilatsophenyo@gmail.com>
2024-03-11 12:05:10 +0200

    Docker and Debugger Setup Complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
+  res.json({ message: 'server running' });
 });
 
 const port = Number(process.env.PORT || 8080);
