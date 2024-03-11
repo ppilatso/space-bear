be71e72d675a7a11afe43e7b2bb7f97de2e1c1fd
Phenyo Pilatso <pilatsophenyo@gmail.com>
2024-03-11 11:05:04 +0200

    add express to project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("server running");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+const port = Number(process.env.PORT || 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
